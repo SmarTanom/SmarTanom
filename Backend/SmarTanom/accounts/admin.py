05cca253c8cf75db1b67b3ce3f5c67ef4a1f45c1
Lien <llagasneilandrew@gmail.com>
2025-05-14 02:09:41 +0800

    Fixed timestamps and adding user

--- Backend/SmarTanom/accounts/admin.py
+++ Backend/SmarTanom/accounts/admin.py
@@ -1,18 +1,19 @@
-# accounts/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ('email', 'name', 'is_admin')
+    list_display = ('email', 'name', 'is_admin', 'is_active', 'created_at')  # Added created_at here
     search_fields = ('email',)
     ordering = ('email',)
+    
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal Info', {'fields': ('name', 'contact')}),
-        ('Permissions', {'fields': ('is_admin', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Timestamps', {'fields': ('created_at', 'updated_at')}),
+        ('Permissions', {'fields': ('is_admin', 'is_staff', 'is_active', 'groups', 'user_permissions')}),
+      
     )
+
 
 admin.site.register(User, CustomUserAdmin)
