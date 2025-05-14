7971f0ce5cac0c30dfc7ce2eef501c3dcdff9d07
IyanuKwent <olandria.iankent@gmail.com>
2025-05-14 22:18:50 +0800

    remove update profile

--- Backend/SmarTanom/SmarTanom/urls.py
+++ Backend/SmarTanom/SmarTanom/urls.py
@@ -5,5 +5,4 @@ from accounts.views import update_profile
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
-    path('api/accounts/update-profile/', update_profile),
 ]
