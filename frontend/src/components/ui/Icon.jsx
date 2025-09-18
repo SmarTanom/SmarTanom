7fbeaf6fbd0de19d386431eec777af6cbb367048
Sosiggg <salas.ivisusej@gmail.com>
2025-09-18 08:32:43 +0800

    feat(icons): add ChevronLeftFilled icon component

--- frontend/src/components/ui/Icon.jsx
+++ frontend/src/components/ui/Icon.jsx
@@ -10,6 +10,23 @@ export const ArrowLeft = (props) => (
   <Svg {...props}><path d="M15 18l-6-6 6-6"/><path d="M3 12h16"/></Svg>
 );
 
+// Filled chevron left (material style)
+export const ChevronLeftFilled = ({ size = 24, color = '#ffffff', ...props }) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill={color}
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    focusable="false"
+    {...props}
+  >
+    <path d="M0 0h24v24H0z" fill="none" />
+    <path d="M15.41 7.41 14 6l-6 6 6 6 1.41-1.41L10.83 12l4.58-4.59z" />
+  </svg>
+);
+
 export const Mail = (props) => (
   <Svg {...props}><rect x="3" y="5" width="18" height="14" rx="2"/><path d="M3 7l9 6 9-6"/></Svg>
 );
@@ -26,4 +43,4 @@ export const Wifi = (props) => (
   <Svg {...props}><path d="M5 12a7 7 0 0 1 14 0"/><path d="M8.5 14.5a4 4 0 0 1 7 0"/><path d="M12 18h0"/></Svg>
 );
 
-export default { ArrowLeft, Mail, Shield, Leaf, Wifi };
+export default { ArrowLeft, Mail, Shield, Leaf, Wifi, ChevronLeftFilled };
